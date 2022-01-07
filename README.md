e1f40597df8430cc6902df31a7ef3df3642eacad
Axel Naumann <Axel.Naumann@cern.ch>
2022-01-07 15:27:06 +0100

    Initial set of files.

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
-# sync-repo-subdir-action
-Apply changes in a repository's subdir to another repository, syncing the latter to the former
+# Sync Repo Subdir Action
+
+Apply changes in a repository's subdir to another repository, syncing the latter to the former.
+Instead of syncing overriding all history, this action builds a changeset (`format-patch`).
+This changeset contains all patches between the previous sync to "now".
+The commit hash of the most recent successful sync is stored in a gist.
+
+## Inputs
+
+### `source`
+
+**Required** `org-name/repo-name/subdir-name`:
+
+- `org-name/repo-name` is the GitHub repo of the commits to apply
+- `subdir-name` is an optional subdirectory; only changes within this directory will be considered
+
+### `source_branch`
+
+**Required** Name of the source repo's branch, default branch (commonly `main` or `master`) if not given.
+
+### `target`
+
+**Required** `org-name/repo-name/subdir-name`:
+
+- `org-name/repo-name` the GitHub repo of the commits to push to
+- `subdir-name` is an optional subdirectory; all changes will be applied below this directory
+
+### `target_branch`
+
+**Optional** Name of the target repo's branch. Matches source branch if not given.
+
+### `gist`
+
+**Required** Gist ID as shown in gist URL, i.e. {id} in https://gist.github.com/{org}/{id}.
+Must exist i.e. [create this first](https://gist.github.com/) with arbitrary content.
+
+### `github_token`
+
+**Required** Secret used to push to the target repo and to update the gist.
+
+## Example usage
+
+```yaml
+uses: root-project/sync-repo-subdir-action@v1
+with:
+  source: "root-project/root/interpreter/cling"
+  source_branch: "master"
+  target: "root-project/cling"
+  gist: "97321608e2aa04e2a6ecbc2b68a41a99"
+  github_token: ${{ secrets.GITHUB_TOKEN }}
+```
---
+++ entrypoint.py
@@ -0,0 +1,218 @@
+#/usr/bin/python3
+
+import os, sys, subprocess
+from github import Github
+
+testing = os.environ["INPUT_TESTING"] 
+if testing:
+  print("::info::Testing mode is on; will reset repositories!")
+
+
+def error_out(kind, msg):
+  print(f"::error::{kind}::{msg}")
+  if testing:
+    rewind_repos()
+  sys.exit(1)
+
+def split_repo_and_dir(repo):
+  """
+  Split the input string
+    org-name/repo-name/subdir-name/more/sub/dirs
+  (where '/subdir-name/more/sub/dirs' is optional) into
+    org-name/repo-name
+  and
+    subdir-name/more/sub/dirs
+  The second part might be the empty string if no subdir-name was given.
+  """
+  parts = repo.split('/')
+  if len(parts) == 1:
+    return [repo, '']
+  return ['/'.join(repo[0:2]), '/'.join(repo[2:])]
+
+
+def get_most_recent_sync_gistfile(github, gist_id):
+  """
+  Return the GistFile named `srsa-last-sync-sha.txt` in gist_id.
+  error_out if the gist cannot be found.
+  Return None if that file cannot be found in the gist.
+  """
+  gist = github.get_gist(gist_id)
+  if not gist:
+    error_out("config", f"Gist with id {gist_id} cannot be found!")
+  try:
+    return gist.files['srsa-last-sync-sha.txt']
+  except:
+    return None
+
+def get_most_recent_sync_sha_and_date_in_gistfile(sha_file, tag):
+  """
+  Return the commit sha of the most recent successful sync, as stored in a
+  GistFile `sha_file`. The line is expected to start with `tag`.
+  Return None if that line cannot be found in the file.
+  """
+  try:
+    sha_line = next(line for line in sha_file.content.splitlines if line.startswith(tag))
+  except:
+    return [None, None]
+  sha_and_date = sha_line[len(tag):]
+  return sha_and_date.split(' *DATE ')
+
+def set_most_recent_sync_sha_and_date_in_gistfile(sha_file, tag, sha):
+  """
+  Store the commit sha of the most recent successful sync and now's daye in a
+  GistFile `sha_file`. The line starts with `tag`.
+  """
+  import datetime
+  from email import utils
+  nowdt = datetime.datetime.now()
+  datestr = utils.format_datetime(nowdt)
+  lines = sha_file.content.splitlines
+  updatedlines = []
+  replaced = False
+  for line in lines:
+    if not line.startswith(tag):
+      updatedlines.append(line)
+  updatedlines.insert(0, tag + sha + ' *DATE ' + datestr)
+  sha_file.content = '\n'.join(updatedlines)
+
+
+
+################################################################################
+print("::group::Config sanity checks")
+
+github_actor = os.environ["GITHUB_ACTOR"] 
+github_token = os.environ["INPUT_GITHUB_TOKEN"]
+g = Github(github_token)
+
+source_repo, source_dir = split_repo_and_dir(os.environ["INPUT_SOURCE"])
+target_repo, target_dir = split_repo_and_dir(os.environ["INPUT_TARGET"])
+
+source_branch = os.environ["INPUT_SOURCE_BRANCH"]
+
+target_branch = source_branch
+if "INPUT_TARGET_BRANCH" in os.environ:
+  target_branch = os.environ["INPUT_TARGET_BRANCH"]
+
+# source = g.get_repo(source_repo)
+target = g.get_repo(target_repo)
+if not target.permissions.push:
+  error_out('config', f"Github token as provided to action does not have permission to push to {target_repo}")
+
+sha_file = get_most_recent_sync_gistfile(g, os.environ["INPUT_GIST"])
+
+print("::endgroup::")
+
+
+################################################################################
+print("::group::Determining previous sync commit")
+
+tag = f"*SOURCE {os.environ['INPUT_SOURCE']} " \
+  + f"*SOURCE_BRANCH {source_branch} " \
+  + f"*TARGET {os.environ['INPUT_TARGET']}" \
+  + f"*TARGET_BRANCH {target_branch} "
+[prev_sha, prev_date] = get_most_recent_sync_sha_and_date_in_gistfile(sha_file, tag)
+
+if prev_sha:
+  print(f"::info::Last sync commit was {prev_sha}")
+else:
+  print(f"::info::Last sync commit not found, assuming first ever sync.")
+
+print("::endgroup::")
+
+
+################################################################################
+print("::group::Checking out source repo")
+
+os.mkdir("source")
+subprocess.run(["git", "init"], cwd="source", check=True)
+subprocess.run(["git", "remote", "add", "origin", f"git@github.com:{source_repo}"], cwd="source", check=True)
+args = ["git", "fetch", "origin", source_branch]
+if prev_date:
+  args.insert(2, f"--shallow-since={prev_date}")
+subprocess.run(args, cwd="source", check=True)
+subprocess.run(["git", "config", "core.sparseCheckout", "true"], cwd="source", check=True)
+with open('source/.git/info/sparse-checkout', 'w') as f:
+    f.write(source_dir)
+subprocess.run(["git", "pull", "origin", source_branch], cwd="source", check=True)
+
+print("::info::source directory after checkout:")
+subprocess.run(["ls"], cwd="source", check=True)
+
+proc_res = subprocess.run(["git", "log", "-1", '--format="format:%H"', "HEAD"], \
+  cwd="source", check=True, capture_output=True)
+source_now_sha = proc_res.stdout.strip()
+print(f"::info::newest source commit: {source_now_sha}")
+
+print("::endgroup::")
+
+
+################################################################################
+print("::group::Getting source patch")
+
+subprocess.run(f"git format-patch --no-stat --find-renames --find-copies --stdout --keep-subject {prev_sha}.. -- {source_dir} > ../patch", \
+    cwd="source", shell=True, check=True)
+have_patch = True
+if os.path.getsize('patch') < 10:
+  print("::info::no patch to apply")
+  have_patch = False
+else:
+  print("::info::patch to apply:\n")
+  with open('patch', 'r') as patch:
+    print(f.read())
+
+print("::endgroup::")
+
+################################################################################
+print("::group::Checking out target repo")
+
+if have_patch:
+  os.mkdir("target")
+  subprocess.run(["git", "init"], cwd="target", check=True)
+  subprocess.run(["git", "remote", "add", "origin", \
+    f"https://{github_actor}:{github_token}@github.com:{target_repo}"], cwd="target", check=True)
+  subprocess.run(["git", "pull", "--depth=1", "origin", target_branch], cwd="target", check=True)
+
+  print("::info::target directory after checkout:")
+  subprocess.run(["ls"], cwd="target", check=True)
+else:
+  print("::info::skipped (no patch)")
+
+print("::endgroup::")
+
+
+################################################################################
+print("::group::Applying patch to target")
+
+if have_patch:
+  dirignore = 1 # 1 for a/ vs b/
+  if source_dir:
+    dirignore += 1 # 1 more for having a "dir"
+  dirignore += source_dir.count('/') # 1 extra for each *sub*dir
+  args = ["git", "am", f"-p{dirignore}", "../patch"]
+  if target_dir:
+    args.append(f"--directory={target_dir}")
+  subprocess.run(args, cwd="target", check=True)
+else:
+  print("::info::skipped (no patch)")
+
+print("::endgroup::")
+
+
+################################################################################
+print("::group::Pushing to target")
+
+if have_patch:
+  subprocess.run(["git", "push", "origin", f"HEAD:{target_branch}"], cwd="target", check=True)
+else:
+  print("::info::skipped (no patch)")
+
+print("::endgroup::")
+
+
+
+################################################################################
+print("::group::Update most recent successful sync commit")
+
+set_most_recent_sync_sha_and_date_in_gistfile(sha_file, tag, source_now_sha)
+
+print("::endgroup::")
